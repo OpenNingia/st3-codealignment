dae394964240601d4c2b5a279383029c5d3e8100
Daniele Simonetti <oppifjellet@gmail.com>
2013-09-10 14:59:03 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-st3-codealignment
-=================
+Code Alignment plugin for Sublime Text 3
+========================================
 
-Code Alignment plugins for Sublime Text 3
+Select the text you want to align and hit CTRL+ALT+=
+
+<p>Code Alignment plugin allow you to convert this code: </p>
+    int magic_number=4;
+    std::string person_name="Mario";
+    static const char* DEFAULT_COUNTRY = "Italy";
+<p>to this: </p>
+    int magic_number                   = 4;
+    std::string person_name            = "Mario";
+    static const char* DEFAULT_COUNTRY = "Italy";
+
+<p>Looks good? You can also choose the separator to use:</p>
+    ![Screenshot](https://raw.github.com/OpenNingia/st3-codealignment/master/screenshot.png "More than a million words")
+
+## Supported Sublime Text versions
+Indent plugin supports Sublime Text 3
+
+## Installation
+In Sublime Text 3 - clone project from [Github](https://github.com/OpenNingia/st3-codealignment.git) into Packages folder.
+
+## Feedback & Support
+Available on [Github](https://github.com/OpenNingia/st3-codealignment)
+
+## Contribution
+...is always welcome! Same place - [Github](https://github.com/OpenNingia/st3-codealignment)
+
+## License
+This software is distributed under GPLv3 license (see LICENSE for details)
---
+++ align_by.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Daniele Simonetti
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import sublime, sublime_plugin
+
+class AlignByCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        w = self.window
+        w.show_quick_panel(
+            ["= align by equal sign", ", align by comma", ": align by colon", "; align by semicolon"],
+            self.on_done)
+
+        self.seps = ['=', ',', ':', ';']
+        self.sep  = '='
+
+    def on_done(self, param):
+        if param >= 0 and param < len(self.seps):
+            self.sep = self.seps[param]
+        self.align_by(self.sep)
+
+    def align_by(self, sep):
+        self.window.active_view().run_command("align_by_separator", {'sep': sep})
+
+class AlignBySeparatorCommand(sublime_plugin.TextCommand):
+    def run(self, edit, sep = '='):
+        self.sep = sep
+        for sel in self.view.sel():
+            self.align_by(edit, sel)
+
+    def align_by(self, edit, reg):
+        text = self.view.substr(reg)
+
+        # calculate
+        max_be = 0
+        tuples = []
+        for l in text.split('\n'):
+            if self.sep not in l:
+                tuples.append( (l,) )
+            else:
+                before_equal, the_sep, after_equal = [x.rstrip() for x in l.partition(self.sep)]
+                if len(before_equal) > max_be:
+                    max_be = len(before_equal)
+                tuples.append( (before_equal, after_equal.lstrip()) )
+
+        # apply
+        out = []
+        for t in tuples:
+            out_fmt = '{{0:{ll}}} {sep} {{1}}'.format(ll=max_be, sep=self.sep)
+            if len(t) == 1:
+                out.append(t[0])
+            else:
+                out.append(out_fmt.format(t[0], t[1]))
+        self.view.replace(edit, reg, '\n'.join(out))
+
